12cd3a22f1092d0091033cf394cf5c7bb345fde6
Petrus Borges Machado <contatopetrusborges@gmail.com>
2023-03-23 22:29:28 -0300

    fix: types component PostsCard

--- src/components/PostsCard/index.tsx
+++ src/components/PostsCard/index.tsx
@@ -1,38 +1,81 @@
 import PostNotFoundImage from '../../assets/icons/postIcon.svg';
 import ShareIcon from '../../assets/icons/shareIcon.svg';
 
-import { PostsCardProps } from '../../types/postsCard';
+import { PostsCardProps, PostModal } from '../../types/postsCard';
 import { Button } from '../Button';
 
 import { Container, Content, PostCard, PostCardComunity } from './styles';
 
+import { Modal } from '../../components/Modal';
+
+import { useState } from 'react';
+
+import { api } from '../../utils/api';
+
+import { ComentaryPostInfo } from '../../types/comentaryPost';
+
 export function PostsCard({ postCard }: PostsCardProps) {
+
+  const [ isVisible, setIsVisible ] = useState(false);
+  const [ postModal, setPostModal ] = useState<null | PostModal>(null);
+  const [ comentaryPost, setComentaryPost ] = useState<ComentaryPostInfo[]>([]);
+
+  async function handleToggleModal(postCardId: number) {
+    try {
+      Promise.all([
+        api.get(`/posts/${postCardId}`),
+        api.get(`/posts/${postCardId}/comments`),
+      ]).then(([postCardIdResponse, comentaryCardIdResponse]) => {
+        setPostModal(postCardIdResponse.data);
+        setComentaryPost(comentaryCardIdResponse.data);
+      });
+    } catch {} finally {
+
+      setIsVisible(true);
+    }
+  }
+
+  function handleCloseModal() {
+    setIsVisible(false);
+  }
+
   return (
-    <Container>
-      <Content>
-        {postCard.map((postCard) => (
-          <PostCard
-            key={postCard.id}
-          >
-            <h2>{postCard.title}</h2>
-            <p>{postCard.body}</p>
-            <img src={PostNotFoundImage} alt="PostNotFoundImage" />
+    <>
+      <Modal
+        isVisible={isVisible}
+        handleCloseModal={handleCloseModal}
+        postModal={postModal}
+        comentaryPost={comentaryPost}
+      />
 
-            <PostCardComunity>
-              <Button
-                type='button'
-              >
+      <Container>
+        <Content>
+          {postCard.map((postCard) => (
+            <PostCard
+              key={postCard.id}
+            >
+              <h2>{postCard.title}</h2>
+              <p>{postCard.body}</p>
+              <img src={PostNotFoundImage} alt="PostNotFoundImage" />
+
+              <PostCardComunity>
+                <Button
+                  type='button'
+                  onClick={() => handleToggleModal(postCard.id)}
+                >
                 Ver comentários
-              </Button>
+                </Button>
 
-              <button
-                type='button'>
-                <img src={ShareIcon} alt="ShareIcon" />
-              </button>
-            </PostCardComunity>
-          </PostCard>
-        ))}
-      </Content>
-    </Container>
+                <button
+                  type='button'
+                >
+                  <img src={ShareIcon} alt="ShareIcon" />
+                </button>
+              </PostCardComunity>
+            </PostCard>
+          ))}
+        </Content>
+      </Container>
+    </>
   );
 }
--- src/types/postsCard.ts
+++ src/types/postsCard.ts
@@ -1,3 +1,10 @@
+export interface PostModal {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
 export interface PostCardInfo {
   userId: number;
   id: number;
