ae1ed2e9f125148d966db6886719b4b68a46c732
Petrus Borges Machado <contatopetrusborges@gmail.com>
2023-03-23 22:46:22 -0300

    fix: import on component AboutInfo

--- src/components/AboutInfo/index.tsx
+++ src/components/AboutInfo/index.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+
 import { SvgTecnologies } from '../SvgTecnologies';
 
 import { Container, Content, ContainerInfo, ContainerTecnologies, ContainerList, AlignSvg, ContainerSocials } from './styles';
