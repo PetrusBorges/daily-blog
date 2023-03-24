ebf153a80027362c3bdfee9b17a763782a8ab216
Petrus Borges Machado <contatopetrusborges@gmail.com>
2023-03-23 22:28:27 -0300

    feat: add component ReactPortal

---
+++ src/components/ReactPortal/index.tsx
@@ -0,0 +1,18 @@
+import { createPortal } from 'react-dom';
+
+interface ReactPortalProps {
+  children: React.ReactNode;
+  containerId: string;
+}
+
+export function ReactPortal({ children, containerId }: ReactPortalProps) {
+  let container = document.querySelector(containerId);
+
+  if (!container) {
+    container = document.createElement('div');
+    container?.setAttribute('id', containerId);
+    document.body.appendChild(container);
+  }
+
+  return createPortal(children, container);
+}
