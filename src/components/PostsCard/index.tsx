7b0491de4b5783feb078646626e6d06f072de97c
Petrus Borges Machado <contatopetrusborges@gmail.com>
2023-03-23 22:38:19 -0300

    fix: add hook usePostsCard on component PostsCard

--- src/components/PostsCard/index.tsx
+++ src/components/PostsCard/index.tsx
@@ -1,43 +1,24 @@
 import PostNotFoundImage from '../../assets/icons/postIcon.svg';
 import ShareIcon from '../../assets/icons/shareIcon.svg';
 
-import { PostsCardProps, PostModal } from '../../types/postsCard';
+import { PostsCardProps } from '../../types/postsCard';
 import { Button } from '../Button';
 
 import { Container, Content, PostCard, PostCardComunity } from './styles';
 
 import { Modal } from '../../components/Modal';
 
-import { useState } from 'react';
-
-import { api } from '../../utils/api';
-
-import { ComentaryPostInfo } from '../../types/comentaryPost';
+import { usePostsCard } from '../../hooks/usePostsCard';
 
 export function PostsCard({ postCard }: PostsCardProps) {
 
-  const [ isVisible, setIsVisible ] = useState(false);
-  const [ postModal, setPostModal ] = useState<null | PostModal>(null);
-  const [ comentaryPost, setComentaryPost ] = useState<ComentaryPostInfo[]>([]);
-
-  async function handleToggleModal(postCardId: number) {
-    try {
-      Promise.all([
-        api.get(`/posts/${postCardId}`),
-        api.get(`/posts/${postCardId}/comments`),
-      ]).then(([postCardIdResponse, comentaryCardIdResponse]) => {
-        setPostModal(postCardIdResponse.data);
-        setComentaryPost(comentaryCardIdResponse.data);
-      });
-    } catch {} finally {
-
-      setIsVisible(true);
-    }
-  }
-
-  function handleCloseModal() {
-    setIsVisible(false);
-  }
+  const {
+    isVisible,
+    postModal,
+    comentaryPost,
+    handleToggleModal,
+    handleCloseModal,
+  } = usePostsCard();
 
   return (
     <>
---
+++ src/hooks/usePostsCard.tsx
@@ -0,0 +1,40 @@
+import { PostModal } from '../types/postsCard';
+
+import { useState } from 'react';
+
+import { api } from '../utils/api';
+
+import { ComentaryPostInfo } from '../types/comentaryPost';
+
+export function usePostsCard() {
+  const [ isVisible, setIsVisible ] = useState(false);
+  const [ postModal, setPostModal ] = useState<null | PostModal>(null);
+  const [ comentaryPost, setComentaryPost ] = useState<ComentaryPostInfo[]>([]);
+
+  async function handleToggleModal(postCardId: number) {
+    try {
+      Promise.all([
+        api.get(`/posts/${postCardId}`),
+        api.get(`/posts/${postCardId}/comments`),
+      ]).then(([postCardIdResponse, comentaryCardIdResponse]) => {
+        setPostModal(postCardIdResponse.data);
+        setComentaryPost(comentaryCardIdResponse.data);
+      });
+    } catch {} finally {
+
+      setIsVisible(true);
+    }
+  }
+
+  function handleCloseModal() {
+    setIsVisible(false);
+  }
+
+  return {
+    isVisible,
+    postModal,
+    comentaryPost,
+    handleToggleModal,
+    handleCloseModal,
+  };
+}
